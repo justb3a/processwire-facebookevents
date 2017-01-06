4bf48540f56ae3f5c4b1a747df0bac94b8c6bfb3
Tabea David <info@justonestep.de>
2017-01-06 17:50:30 +0100

    initial commit

---
+++ FacebookEventsConfig.php
@@ -0,0 +1,92 @@
+<?php namespace ProcessWire;
+
+/**
+ * Class FacebookEventsConfig
+ */
+class FacebookEventsConfig extends ModuleConfig {
+
+  /**
+   * array Default config values
+   */
+  public function getDefaults() {
+    return array(
+      'clientId' => '',
+      'clientSecret' => '',
+      'pageId' => '',
+      'pageName' => '',
+      'cacheExpire' => 'daily'
+    );
+  }
+
+  /**
+   * Retrieves the list of config input fields
+   * Implementation of the ConfigurableModule interface
+   *
+   * @return InputfieldWrapper
+   */
+  public function getInputfields() {
+    // get submitted data
+    $cacheExpire = isset($this->data['cacheExpire']) ? $this->data['cacheExpire'] : 'daily';
+
+    $inputfields = parent::getInputfields();
+
+    // field app ID
+    $field = $this->modules->get('InputfieldText');
+    $field->name = 'clientId';
+    $field->label = __('Facebook App ID');
+    $field->columnWidth = 50;
+    $field->required = 1;
+    $inputfields->add($field);
+
+    // field app secret
+    $field = $this->modules->get('InputfieldText');
+    $field->name = 'clientSecret';
+    $field->label = __('Facebook App Secret');
+    $field->columnWidth = 50;
+    $field->required = 1;
+    $inputfields->add($field);
+
+    // field page Name
+    $field = $this->modules->get('InputfieldText');
+    $field->name = 'pageName';
+    $field->label = __('Facebook Page Name');
+    $field->description = __('You can either enter the facebook page name or ID.');
+    $field->notes = __('https://www.facebook.com/XXX/');
+    $field->required = 1;
+    $field->columnWidth = 50;
+    $inputfields->add($field);
+
+    // field page ID
+    $field = $this->modules->get('InputfieldText');
+    $field->name = 'pageId';
+    $field->label = __('Facebook Page ID');
+    $field->columnWidth = 50;
+    $field->collapsed = Inputfield::collapsedNoLocked;
+    $inputfields->add($field);
+
+    // Fb Access Token
+    $field = $this->modules->get('InputfieldText');
+    $field->name = 'accessToken';
+    $field->label = __('Facebook Access Token');
+    $field->columnWidth = 50;
+    $field->collapsed = Inputfield::collapsedHidden;
+    $inputfields->add($field);
+
+    // field cache ID
+    $field = $this->modules->get('InputfieldSelect');
+    $field->label = 'Cache expires';
+    $field->description = __('By default a cache lasts for one day. You could select another lifetime.');
+    $field->attr('name', 'cacheExpire');
+    $field->attr('value', $cacheExpire);
+    $field->columnWidth = 100;
+    $field->required = 1;
+    $lifetimes = array('never', 'save', 'now', 'hourly', 'daily', 'weekly', 'monthly');
+    foreach($lifetimes as $lifetime) {
+      $field->addOption($lifetime, $lifetime);
+    }
+    $inputfields->add($field);
+
+    return $inputfields;
+  }
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,72 @@
-# processwire-facebookevents
+# ProcessWire FacebookEvents
+
 ProcessWire module to get Facebook Page Events using the Graph API.
+
+## Create an app on Facebook developers website
+
+You have to create an app to get appId and appSecret. Those keys are required.  
+Go to [Facebook Developers](https://developers.facebook.com/) and add a new app.  
+Congrats! Now you can copy your Facebook appId and appSecret.
+
+## Get your Facebook page ID 
+
+You can either enter your facebook page ID or the facebook page name.  
+If you enter the Facebook page name, this module will get the page ID for you!
+
+## Call Module
+
+```php
+$events = $modules->get('FacebookEvents')->getEvents();
+```
+
+## Output Events
+
+```twig
+{% for event in events|reverse %}
+  {% if event.start_time|date('U') > date().timestamp %}
+    <div>
+      {% set dts = modules.get('FacebookEvents').getDates(event) %}
+      <a href="https://www.facebook.com/events/{{event.id}}/" title="Facebook">{{dts.dates}}:</a>
+      {{event.name}} <em>{{dts.times}}</em>
+    </div>
+  {% endif %}
+{% endfor %}
+```
+
+## Format / Combine Start and End Date
+
+```php
+$dts = $modules->get('FacebookEvents')->getDates($event);
+```
+
+**Parameter:**
+
+| param       | type   | required | default | description           |
+|-------------|--------|----------|---------|-----------------------|
+| $event      | array  | true     | /       | current event (loop)  |
+| $formatDate | string | false    | d.m.Y   | date format           |
+| $formatTime | string | false    | H:i     | time format           |
+| $divider    | string | false    | -       | date / time separator |
+
+**Result:**
+
+array with keys:
+
+- dates
+- times
+
+**Example Output:**
+
+```html
+<!-- one-day event -->
+<div>
+  <a href="https://www.facebook.com/events/xxx/" title="xxx">07.01.2017:</a>
+  event title <em>12:00 - 16:00Uhr</em>
+</div>
+
+<!-- multi-day event -->
+ <div>
+  <a href="https://www.facebook.com/events/xxx/" title="xxx">01.07.2017 - 02.07.2017:</a>
+  another event title <em>09:30 - 17:00</em>
+</div>
+```
